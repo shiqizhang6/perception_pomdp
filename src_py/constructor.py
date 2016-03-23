e399094e379963f562ab94af73e27400c5e64eb4
Shiqi Zhang <shiqi.zhang6@gmail.com>
2016-03-22 21:18:12 -0500

    a bug-free version; to be tested

--- src_py/constructor.py
+++ src_py/constructor.py
@@ -12,7 +12,7 @@ class State(object):
         self._name = self.prop_values_to_str()
 
     def prop_values_to_str(self): 
-        if term is True:
+        if self._term is True:
             return 'terminal'
         else:
             return 's'+str(self._s_index)+'p'+''.join(self._prop_values)
@@ -44,10 +44,11 @@ class Obs(object):
 
 class Model:
 
-    def __init__(self, discount, prop_names): 
+    def __init__(self, discount, prop_names, high_acc): 
         self._discount = discount
         self._num_comp_states = 7
         self._prop_names = prop_names
+        self._high = high_acc
 
         self.generate_state_set()
         self.generate_action_set()
@@ -62,26 +63,34 @@ class Model:
 
         self._states = []
 
-        depth = len(self._prop_names)
         for i in range(self._num_comp_states):
-            self.generate_state_set_helper(i, 0, [], depth)
+            self.generate_state_set_helper(i, 0, [], len(self._prop_names))
 
         self._states.append(State(True, None, None))
 
+        # print(str([s._name for s in self._states]))
+        # exit(1)
+
     def generate_state_set_helper(self, s_index, curr_depth, path, depth):
         
+        # print('s_index: ' + str(s_index))
+        # print('curr_depth: ' + str(curr_depth))
+        # print('path: ' + str(path))
+        # print('depth: ' + str(depth))
+        # print
         if len(path) == depth:
-            self._states.append(State(False, i, path))
+            self._states.append(State(False, s_index, path))
+            return
 
-        self.generate_state_set_helper(s_index, curr_depth+1, path.append('0'), depth)
-        self.generate_state_set_helper(s_index, curr_depth+1, path.append('1'), depth)
+        self.generate_state_set_helper(s_index, curr_depth+1, path+['0'], depth)
+        self.generate_state_set_helper(s_index, curr_depth+1, path+['1'], depth)
 
     def get_state_index(self, term, s_index, prop_values):
         if term == True:
             return len(self._states) - 1
 
         else:
-            return s_index*pow(2, len(prop_values)) + int(''.join(path), 2) - 1
+            return s_index*pow(2, len(prop_values)) + int(''.join(prop_values), 2) - 1
 
     def generate_action_set(self):
 
@@ -99,25 +108,33 @@ class Model:
 
         self.generate_action_set_helper(0, [], len(self._prop_names))
 
+        # print(str([a._name for a in self._actions]))
+        # exit(1)
+
     def generate_action_set_helper(self, curr_depth, path, depth):
         if len(path) == depth:
             self._actions.append(Action(True, None, path))
+            return
 
-        self.generate_action_set_helper(curr_depth+1, path.append('0'), depth)
-        self.generate_action_set_helper(curr_depth+1, path.append('1'), depth)
+        self.generate_action_set_helper(curr_depth+1, path+['0'], depth)
+        self.generate_action_set_helper(curr_depth+1, path+['1'], depth)
 
     def generate_observation_set(self):
 
         self._observations = []
-        self.generate_observation_set_helper(0, [], len(self._prop_name))
-        self._observations.append(True, None)
+        self.generate_observation_set_helper(0, [], len(self._prop_names))
+        self._observations.append(Obs(True, None))
+
+        # print(str([o._name for o in self._observations]))
+        # exit(1)
 
     def generate_observation_set_helper(self, curr_depth, path, depth):
         if len(path) == depth:
             self._observations.append(Obs(False, path))
+            return
 
-        self.generate_observation_set_helper(curr_depth+1, path.append('0'), depth)
-        self.generate_observation_set_helper(curr_depth+1, path.append('1'), depth)
+        self.generate_observation_set_helper(curr_depth+1, path+['0'], depth)
+        self.generate_observation_set_helper(curr_depth+1, path+['1'], depth)
 
     def generate_trans_fun(self):
 
@@ -127,7 +144,7 @@ class Model:
             if a_val._name == 'look':
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 0:
-                        tmp_s_idx = get_state_index(False, 1, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 1, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = 1.0
                     else:
                         self._trans[a_idx, s_idx, s_idx] = 1.0
@@ -138,7 +155,7 @@ class Model:
                 success_push = 0.9
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 1:
-                        tmp_s_idx = get_state_index(False, 6, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 6, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -147,7 +164,7 @@ class Model:
                 success_push = 0.9
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 1:
-                        tmp_s_idx = get_state_index(False, 2, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 2, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -156,7 +173,7 @@ class Model:
                 success_push = 0.9
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 2:
-                        tmp_s_idx = get_state_index(False, 3, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 3, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -165,7 +182,7 @@ class Model:
                 success_push = 0.99
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 3:
-                        tmp_s_idx = get_state_index(False, 4, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 4, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -174,7 +191,7 @@ class Model:
                 success_push = 0.99
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 4:
-                        tmp_s_idx = get_state_index(False, 5, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 5, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -183,7 +200,7 @@ class Model:
                 success_push = 0.9
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 5:
-                        tmp_s_idx = get_state_index(False, 6, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 6, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -192,7 +209,7 @@ class Model:
                 success_push = 1.0
                 for s_idx, s_val in enumerate(self._states):
                     if s_val._term == False and s_val._s_index == 6:
-                        tmp_s_idx = get_state_index(False, 0, s_val._prop_values)
+                        tmp_s_idx = self.get_state_index(False, 0, s_val._prop_values)
                         self._trans[a_idx, s_idx, tmp_s_idx] = success_push
                         self._trans[a_idx, s_idx, s_idx] = 1.0 - success_push
                     else:
@@ -213,7 +230,6 @@ class Model:
             else:
                 self.dic[words[1]] = {words[0]: [int(w)+1 for w in words[2:]]}
 
-
     def generate_obs_fun(self):
 
         self._obs_fun = np.zeros((len(self._actions), len(self._states), len(self._observations)))
@@ -223,11 +239,30 @@ class Model:
 
         for a_idx, a_val in enumerate(self._actions):
             for s_idx, s_val in enumerate(self._states):
+
+                if a_val._term == True or a_val._name == 'reinit' or s_val._term == True:
+                    self._obs_fun[a_idx, s_idx, len(self._observations)-1] = 1.0
+                    continue
+
                 for o_idx, o_val in enumerate(self._observations):
+
                     prob = 1.0
+                    if o_val._nonavail == True:
+                        self._obs_fun[a_idx, s_idx, o_idx] = prob
+                        continue 
+
+                    if a_val._name == 'ask':
+                        if s_val._prop_values == o_val._prop_values:
+                            self._obs_fun[a_idx, s_idx, o_idx] = self._high
+                        else:
+                            self._obs_fun[a_idx, s_idx, o_idx] = \
+                            (1.0 - self._high)/(len(self._observations)-2.0)
+                        continue
+
                     for p_s_idx, p_s_val in enumerate(s_val._prop_values):
+
                         p_o_val = o_val._prop_values[p_s_idx]
-                        mat = self.dic[a_val._name][self.prop_names[p_s_idx]]
+                        mat = self.dic[a_val._name][self._prop_names[p_s_idx]]
                         if p_s_val == '0' and p_o_val == '0':
                             prob = prob * mat[3]/(mat[1] + mat[3])
                         elif p_s_val == '0' and p_o_val == '1':
@@ -236,15 +271,17 @@ class Model:
                             prob = prob * mat[2]/(mat[0] + mat[2])
                         elif p_s_val == '1' and p_o_val == '1':
                             prob = prob * mat[0]/(mat[0] + mat[2])
+
                     self._obs_fun[a_idx, s_idx, o_idx] = prob
+                    print('prob: ' + str(prob))
 
     def generate_reward_fun(self):
         self._reward_fun = np.zeros((len(self._actions), len(self._states)))
         for a_idx, a_val in enumerate(self._actions):
             for s_idx, s_val in enumerate(self._states):
-                if a_val.term == False:
+                if a_val._term == False:
                     self._reward_fun[a_idx, s_idx] = -2.0
-                elif s_val.term == True:
+                elif s_val._term == True:
                     self._reward_fun[a_idx, s_idx] = 0.0
                 elif a_val._prop_values == s_val._prop_values:
                     self._reward_fun[a_idx, s_idx] = 100.0
@@ -268,7 +305,7 @@ class Model:
         s += '\n\n'
 
         for a in range(len(self._actions)):
-            s += 'T: ' + a._name + '\n'
+            s += 'T: ' + self._actions[a]._name + '\n'
             for s1 in range(len(self._states)):
                 for s2 in range(len(self._states)):
                     s += str(self._trans[a, s1, s2]) + ' '
@@ -276,7 +313,7 @@ class Model:
             s += '\n'
 
         for a in range(len(self._actions)):
-            s += 'O: ' + a._name + '\n'
+            s += 'O: ' + self._actions[a]._name + '\n'
             for s1 in range(len(self._states)):
                 for o in range(len(self._observations)):
                     s += str(self._obs_fun[a, s1, o]) + ' '
@@ -285,7 +322,7 @@ class Model:
 
         for a in range(len(self._actions)):
             for s1 in range(len(self._states)):
-                s += 'R: ' + a._name + ' : ' + s1._name + ' : * : * '
+                s += 'R: ' + self._actions[a]._name + ' : ' + self._states[s1]._name + ' : * : * '
                 s += str(self._reward_fun[a, s1]) + '\n'
 
         f = open(path, 'w')
@@ -293,7 +330,7 @@ class Model:
 
 def main(argv):
     
-    model = Model(0.95, ['light', 'talk'])
+    model = Model(0.95, ['light', 'tall'], 0.9)
     model.write_to_file('model.POMDP')
 
 if __name__ == "__main__":
